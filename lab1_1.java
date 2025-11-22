7dad7197fe7c6884257fe8437d43c0610da681ca
Burapa <guy6176@gmail.com>
2025-11-22 21:06:43 +0700

    Update lab1_1.java

--- lab1_1.java
+++ lab1_1.java
@@ -4,15 +4,27 @@ import java.util.Scanner ;
 
 public class lab1_1 {
     public static void main(String[] args) {
-        try (Scanner sc = new Scanner(System.in)) {
-            System.out.print(" กรอกตัวเลข a : ") ;
-            int a = sc.nextInt() ;
+        Scanner input = new Scanner(System.in) ;
 
-            System.out.print(" กรอกตัวเลข b : ") ;
-            int b = sc.nextInt() ;
+        int num1 = readInt(input, "Enter Num1: ") ;
+        int num2 = readInt(input, "Enter Num2: ") ;
 
-            int sum = a + b ;
-            System.out.println( "ผลรวม = " + sum ) ;
+        int result = num1 + num2;
+        System.out.println(num1 + " + " + num2 + " = " + result) ;
+
+        input.close() ;
+    }
+
+    private static int readInt(Scanner input, String prompt) {
+        while (true) {
+            System.out.print(prompt) ;
+            if (input.hasNextInt()) {
+                int value = input.nextInt() ;
+                return value ;
+            } else {
+                System.out.println("Enter only Number!!!") ;
+                input.next() ;
+            }
         }
     }
 }
